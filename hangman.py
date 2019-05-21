95f8d2bd02b2ae317d7fe2a8511ef71a3bd75298
Arun Pendyala <arunpendyala001@gmail.com>
2019-05-22 03:17:47 +0530

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,10 +1,13 @@
 import random
 
-a = int(input("Select a category\n1 Movies\n2 Cars \n"))
-Movies = ["DARLING", "AVENGERS"]
-Cars = ["FORD", "BENTLEY", "AUDI"]
-if a == 1:
-	b = Movies[random.randint(0,(len(Movies)-1))]
+def chances(c):
+	if c == 0:
+		print("You've lost try again")
+	else:
+		print("You've won!")
+
+def work(choice):
+	b = choice[random.randint(0,(len(choice)-1))]
 	chance = 7
 	guess = [ ]
 	for i in range (len(b)):
@@ -12,14 +15,10 @@ if a == 1:
 	print(*guess)
 	print("Start guessing")
 
-
 	while chance>0 and "_" in guess:
-
 		print(chance,"lives left")
 		cha = input("Enter your choice: ")
-		# list(b)
 		found = False
-		print(b)
 		for idx,val in enumerate(b):
 			if cha.capitalize() == val:
 				guess[idx]=cha.capitalize()
@@ -29,16 +28,17 @@ if a == 1:
 
 		print(*guess)
 
-	if chance == 0:
-		print("You've lost try again")	
-	else:
-		print("you won")
+	chances(chance)
 
 
 
 
+a = int(input("Select a category\n1 Movies\n2 Cars \n"))
+Movies = ["DARLING", "AVENGERS"]
+Cars = ["FORD", "BENTLEY", "AUDI"]
 
+if a == 1:
+	work(Movies)
 
 elif a == 2:
-	c = Cars[random.randint(0,(len(Cars)-1))]
-	print(c)
+	work(Cars)
